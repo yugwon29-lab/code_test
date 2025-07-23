c7d20484c0f593c1d007cd311fbb0d954496f0c9
Yugwon Seo <yugwon29@gmail.com>
2025-07-23 15:44:54 +0900

    5108

---
+++ swea/swea_5108.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+
+    # 5108. 숫자 추가
+    N, M, L = map(int, input().split())
+    num_row = [int(x) for x in input().split()]
+    for i in range(M):
+        idx, n = map(int, input().split())
+        num_row = num_row[:idx] + [n] + num_row[idx:]
+
+    print(f'#{test_case} {num_row[L]}')
+
+
