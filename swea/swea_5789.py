d691a249d3f29639e3520c3cb3a0deed611cb1d3
Yugwon Seo <yugwon29@gmail.com>
2025-07-24 17:53:46 +0900

    s

--- swea/swea_5789.py
+++ swea/swea_5789.py
@@ -3,32 +3,18 @@ T = int(input())
 for test_case in range(1, T + 1):
 
     # 5789. 현주의 상자 바꾸기
-    N = int(input())
-    numbers = [int(x) for x in input().split()]
+    N, Q = map(int, input().split())
 
-    tree = [None] * (N+1)
-    for i, n in enumerate(numbers):
-        idx = i + 1
-        tree[idx] = n
-        print(tree)
-        while True:
-            if idx // 2 == 0:
-                break
-            else:
-                if tree[idx // 2] > tree[idx]:
-                    tree[idx // 2], tree[idx] = tree[idx], tree[idx // 2]
-                idx = idx // 2
-    
-    print(tree)
-    sum = 0
-    idx = N
-    while True:
-        if idx // 2 == 0:
-            break
+    boxes = [0] * (N+1)
+
+    for q in range(Q):
+        L, R = map(int, input().split())
+        boxes[L:R+1] = [q+1] * (R-L+1)
+
+    for i in range(len(boxes)):
+        if i == 0:
+            print(f'#{test_case}', end=' ')
+        elif i == len(boxes) - 1:
+            print(boxes[i])
         else:
-            sum += idx[N]
-        idx = idx // 2
-    
-    print(f'#{test_case} {sum}')
-
-
+            print(boxes[i], end=' ')
